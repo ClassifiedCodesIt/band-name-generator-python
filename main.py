438988fa5197bdeec038c41e03adbb661887a039
ClassifiedCodesIt <148991833+ClassifiedCodesIt@users.noreply.github.com>
2025-01-07 17:26:56 +0000

    Created file

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Classified Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your Band Name could be " + city + " " + pet_name)
